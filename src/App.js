28247a865dcb7c2d7d0b97cfad715979782696d4
Lex-Llanes <95273812+Lex-Llanes@users.noreply.github.com>
2022-03-04 14:27:06 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import RegisterYourApplicationForm from "./components/form";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Techtonica</h1>
+      <RegisterYourApplicationForm />
+    </div>
+  );
+}
